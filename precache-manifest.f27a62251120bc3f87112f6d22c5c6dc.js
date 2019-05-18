86093f5d616ae3a4ce813dab1c80c36a5b6d65f0
Ryan Davis <ryanscdavis@gmail.com>
2019-05-18 15:54:59 -0400

    Updates

---
+++ precache-manifest.f27a62251120bc3f87112f6d22c5c6dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9e6cd398cbe93a8b8919a5451de51ba",
+    "url": "/d3-pie-hue/index.html"
+  },
+  {
+    "revision": "fa039e2dab5cb25cfca9",
+    "url": "/d3-pie-hue/static/css/main.113fec22.chunk.css"
+  },
+  {
+    "revision": "aa73e75358588a3a7002",
+    "url": "/d3-pie-hue/static/js/2.1e3ad570.chunk.js"
+  },
+  {
+    "revision": "fa039e2dab5cb25cfca9",
+    "url": "/d3-pie-hue/static/js/main.8020a42f.chunk.js"
+  },
+  {
+    "revision": "8d45ba8f293ca42df488",
+    "url": "/d3-pie-hue/static/js/runtime~main.f26c8290.js"
+  }
+]);
